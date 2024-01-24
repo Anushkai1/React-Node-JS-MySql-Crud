502c9835c55b8612f6e642ce6a6264dc6a66f572
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-01-24 08:40:31 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Books/> }/>
           <Route path="/add" element={<Add/> }/>
-          <Route path="/update/:id" element={<Updat/> }/>
+          <Route path="/update/:id" element={<Update/> }/>
         </Routes>
       </BrowserRouter>
     </div>
